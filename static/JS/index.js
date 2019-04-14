40f869b2e085e182a40f730cd6f4a28066e37757
dpalmieri1 <palmierid16@students.ecu.edu>
2019-04-13 22:06:47 -0400

    Tic-Tac-Toe

--- static/JS/index.js
+++ static/JS/index.js
@@ -1,32 +1,62 @@
-var board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-
-var player, sqrId, user, computer, row, col;
+var sqrId, user, SIZE = 8;
 const ARR_LENGTH = 9;
+
+function Create2DArray(rows) {
+  var arr = [];
+
+  for (var i=0;i<rows;i++) {
+     arr[i] = [];
+  }
+
+  return arr;
+}
+
+var board = Create2DArray(SIZE);
+
+function GetIndex(num){
+    var n1 = num%SIZE;
+    var n2 = num%SIZE;
+    return [n1, n2];
+}
+
+function clear(n1,n2,id){
+
+}
+
+function flag(n1,n2,id){
+
+}
+
+function PlaceBombs(){
+
+}
+
+
+
 $(document).ready(function() {
   //1 checkbox event listener
   $(".checkBox").click(function() {
     if($(this).is(":checked")) {
       user = $(this).val();
-      player = user;
-      computer = (user == 'X') ? 'O' : 'X';
     }
   });
   
   //2 square even listener 
   $(".square").click(function() {
-    console.log(board[0]);
-    sqrId = $(this).attr("id");
-    playerMove();
-    // computerAI();
-    minimax(board);
-    if(checkWinner(board)) {
-      alert(player+" Wins the game!");
-      resetBoard();
-    }    
-    if(checkDraw(board)) {
-      alert("It's a draw!");
-    } 
-    // player = (player == user) ? computer : user;
+    //console.log(board[0]);
+    sqrID = $(this).attr("id");
+
+    var codes = GetIndex(sqrID);
+    var row = codes[0];
+    var col = codes[1];
+
+    if(user=="X"){
+        clear(row,col,sqrID);
+    }
+    else{
+        flag(row,col,sqrID);
+    }
+
   });
   
   //reset board
@@ -35,6 +65,8 @@ $(document).ready(function() {
   })
   
 });
+
+
 
 //player move
 function playerMove() {
@@ -48,29 +80,6 @@ function playerMove() {
     }    
 }
 
-
-/* computer AI generate random number between 0 - 8  */
-function computerAI() {
-  var random;
-  var min = 0, max = 8;
-  do {
-    random = Math.floor(Math.random() * (max + min));
-  }while($("#"+random).text() != "")
-  $("#"+random).text(computer);
-  // row = getRow();
-  // col = getCol();
-  board[random] = computer;
-}
-
-//getting row number 
-function getRow() {
-  return Math.floor(sqrId / ARR_LENGTH);
-}
-
-//getting col number
-function getCol() {
-  return sqrId % ARR_LENGTH;
-}
 
 /* checking for winner */
 // winning combinations using the board indexies
@@ -89,14 +98,6 @@ function checkWinner(board){
  return false;
 }
 
-function checkDraw(board) {
-  for(var i = 0; i < board.length; i++) {
-    if(board[i] !== 'X' && board[i] !== 'O')
-      return false;
-  }
-  return true;
-}
-
 function resetBoard() {
   $(".square").text("");
   $(".checkBox").prop("checked", false); 
@@ -106,53 +107,4 @@ function resetBoard() {
   for(var i = 0; i < ARR_LENGTH; i++) {
     board[i] = "";
   }
-}
-
-// the main minimax function
-function minimax(board, depth, player) {
-  console.log(player+" "+user+" "+computer);
-  console.log("board before filter: "+board)
-	//making a copy of empty spaces in board	
-	var newBoard = board.filter(val =>
-    val != 'X' && val != 'O'
-  );
-  
-  console.log("board after filter: "+newBoard);
-	//if no winners keep playing
-	if(!checkWinner(newBoard)) {
-		var values = []; //holding value of plays
-    
-    if(player == computer) {
-      //looking for available squares and calling minimax
-      for(var i = 0; i < newBoard.length; i++) {
-        //playing to available spot
-        newBoard[i] = computer;
-        //calling minimax
-        const value = minimax(newBoard, depth+1, player);
-        var maxVal = Math.max(maxVal, value);
-        return maxVal;
-      }
-    }
-		
-
-		//return the value if player is computer
-		if(player == computer) {
-			return Math.max(values)
-		}
-		else {
-			return Math.min(values)
-		}				
-
-	}
-	//if computer return positive number
-	else if(checkWinners() && player == computer) {
-		return 10;
-	}
-	//if user return negative number
-	else if(checkWinners() && player == user) {
-		return -10;
-	}
-	else if(checkDraw()) {
-		return null;
-	}
 }
