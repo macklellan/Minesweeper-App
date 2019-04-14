9fd338776932dcbda6ec3b8bf08bd082aec63ea3
dpalmieri1 <palmierid16@students.ecu.edu>
2019-04-13 22:29:44 -0400

    place bombs

--- static/JS/index.js
+++ static/JS/index.js
@@ -1,17 +1,22 @@
-var sqrId, user, SIZE = 8;
+var sqrId, user, SIZE = 8, BOMBS = 10;
 const ARR_LENGTH = 9;
 
-function Create2DArray(rows) {
-  var arr = [];
-
-  for (var i=0;i<rows;i++) {
-     arr[i] = [];
-  }
-
-  return arr;
+function PrintArray(A){
+        console.log(A);
 }
 
-var board = Create2DArray(SIZE);
+function Create2DArray() {
+    var arr = [];
+
+    for (var i=0;i<SIZE;i++) {
+        arr[i] = [];
+        for (var j=0;j<SIZE;j++) {
+            arr[i][j] = 0;
+        }
+    }
+
+    return arr;
+}
 
 function GetIndex(num){
     var n1 = num%SIZE;
@@ -28,83 +33,97 @@ function flag(n1,n2,id){
 }
 
 function PlaceBombs(){
+    var i=0;
+    var n1, n2;
+    while(i<BOMBS){
+        n1 = Math.floor((Math.random() * 8));
+        n2 = Math.floor((Math.random() * 8));
+        if(board[n1][n2]== 0){
+            board[n1][n2] = 1;
+            i++;
+        }
+    }
 
+    console.log(board)
 }
 
+var board = Create2DArray(SIZE);
+var guess = Create2DArray(SIZE);
 
+PlaceBombs();
 
 $(document).ready(function() {
-  //1 checkbox event listener
-  $(".checkBox").click(function() {
-    if($(this).is(":checked")) {
-      user = $(this).val();
-    }
-  });
-  
-  //2 square even listener 
-  $(".square").click(function() {
-    //console.log(board[0]);
-    sqrID = $(this).attr("id");
+    //1 checkbox event listener
+    $(".checkBox").click(function() {
+        if($(this).is(":checked")) {
+            user = $(this).val();
+        }
+    });
 
-    var codes = GetIndex(sqrID);
-    var row = codes[0];
-    var col = codes[1];
+    //2 square even listener
+    $(".square").click(function() {
+        //console.log(board[0]);
+        sqrID = $(this).attr("id");
 
-    if(user=="X"){
-        clear(row,col,sqrID);
-    }
-    else{
-        flag(row,col,sqrID);
-    }
+        var codes = GetIndex(sqrID);
+        var row = codes[0];
+        var col = codes[1];
 
-  });
-  
-  //reset board
-  $(".reset").click(function() {
-    resetBoard();
-  })
-  
+        if(user=="X"){
+            clear(row,col,sqrID);
+        }
+        else{
+            flag(row,col,sqrID);
+        }
+
+    });
+
+    //reset board
+    $(".reset").click(function() {
+        resetBoard();
+    })
+
 });
 
 
 
 //player move
 function playerMove() {
-  if($("#"+sqrId).text() == "") {
-      $("#"+sqrId).text(player);
-      board[sqrId] = player;   
-      console.log(board);
+    if($("#"+sqrId).text() == "") {
+        $("#"+sqrId).text(player);
+        board[sqrId] = player;
+        console.log(board);
     }
     else {
-      alert("Wrong move");
-    }    
+        alert("Wrong move");
+    }
 }
 
 
 /* checking for winner */
 // winning combinations using the board indexies
 function checkWinner(board){
- if (
- (board[0] == player && board[1] == player && board[2] == player) ||
- (board[3] == player && board[4] == player && board[5] == player) ||
- (board[6] == player && board[7] == player && board[8] == player) ||
- (board[0] == player && board[3] == player && board[6] == player) ||
- (board[1] == player && board[4] == player && board[7] == player) ||
- (board[2] == player && board[5] == player && board[8] == player) ||
- (board[0] == player && board[4] == player && board[8] == player) ||
- (board[2] == player && board[4] == player && board[6] == player)) {
- return true;
- } 
- return false;
+    if (
+        (board[0] == player && board[1] == player && board[2] == player) ||
+        (board[3] == player && board[4] == player && board[5] == player) ||
+        (board[6] == player && board[7] == player && board[8] == player) ||
+        (board[0] == player && board[3] == player && board[6] == player) ||
+        (board[1] == player && board[4] == player && board[7] == player) ||
+        (board[2] == player && board[5] == player && board[8] == player) ||
+        (board[0] == player && board[4] == player && board[8] == player) ||
+        (board[2] == player && board[4] == player && board[6] == player)) {
+        return true;
+    }
+    return false;
 }
 
 function resetBoard() {
-  $(".square").text("");
-  $(".checkBox").prop("checked", false); 
-  user = "";
-  turn = "";
-  computer = "";
-  for(var i = 0; i < ARR_LENGTH; i++) {
-    board[i] = "";
-  }
+    $(".square").text("");
+    $(".checkBox").prop("checked", false);
+    user = "";
+    turn = "";
+    computer = "";
+    for(var i = 0; i < ARR_LENGTH; i++) {
+        board[i] = "";
+    }
 }
